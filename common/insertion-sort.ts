d7d469f909383aed6aa7a088a8b4022c4911dd9d
anax015obs <our4000@gmail.com>
2021-04-15 10:07:21 +0900

    ...

---
+++ common/insertion-sort.ts
@@ -0,0 +1,14 @@
+const sort = (A: number[]) => {
+  for (let j = 1; j < A.length; j++) {
+    const key = A[j];
+    let i = j - 1;
+    while (i >= 0 && A[i] > key) {
+      A[i + 1] = A[i];
+      i = i - 1;
+    }
+    A[i + 1] = key;
+  }
+  return A;
+};
+
+export default sort;
---
+++ index.ts
@@ -0,0 +1,6 @@
+import insertionSort from "./common/insertion-sort";
+
+const arr = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+console.log(`before: `, arr);
+console.log(`after: `, insertionSort(arr));
