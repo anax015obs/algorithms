cb30fbf38d2fdf9ad4eea5469d853944a7e6766f
anax015obs <our4000@gmail.com>
2021-04-15 10:09:37 +0900

    ...

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var insertion_sort_1 = __importDefault(require("./insertion-sort"));
-var arr = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-console.log("before: ", arr);
-console.log("after: ", insertion_sort_1.default(arr));
